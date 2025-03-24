80c095d2ec140e1ac22334c461ef9d99b8d3c870
Marina Nicolai <nicolaimarina@gmail.com>
2025-03-24 14:21:03 -0400

    fix the bug

--- src/components/BlogFeed.jsx
+++ src/components/BlogFeed.jsx
@@ -1,70 +1,68 @@
-import React, { useCallback, useEffect, useRef } from 'react'
-import { useSelector } from 'react-redux'
+import React, { useCallback, useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
 
 function BlogFeed({ blogFeedType2 = false }) {
-   const { blogs = [], blogFeed = [], blogFeed2 = [] } = useSelector(state => state.blogs || {})
+  const { blogs = [], blogFeed = [], blogFeed2 = [] } = useSelector((state) => state.blogs || {});
 
-   const data = blogFeedType2
-      ? blogFeed2.map(blog => ({
-           ...blogs.find(e => e.id === blog.blogId),
-           ...blog,
-        }))
-      : blogFeed.map(blog => ({
-           ...blogs.find(e => e.id === blog.blogId),
-           ...blog,
-        }))
+  const data = blogFeedType2
+    ? blogFeed2.map((blog) => {
+        const blogData = blogs.find((e) => e.id === blog.blogId);
+        return blogData ? { ...blogData, ...blog } : null;
+      }).filter(Boolean)
+    : blogFeed.map((blog) => {
+        const blogData = blogs.find((e) => e.id === blog.blogId);
+        return blogData ? { ...blogData, ...blog } : null;
+      }).filter(Boolean);
 
-   const container1Ref = useRef(null)
-   const container2Ref = useRef(null)
+  const container1Ref = useRef(null);
+  const container2Ref = useRef(null);
 
-   const handleScroll = useCallback(() => {
-      if (container1Ref.current && container2Ref.current) {
-         const elements = [...container1Ref.current.children, ...container2Ref.current.children]
+  const handleScroll = useCallback(() => {
+    if (!container1Ref.current || !container2Ref.current) return;
 
-         elements.forEach(e => {
-            const top = e.getBoundingClientRect().top
-            const bottom = e.getBoundingClientRect().bottom
+    const elements = [...container1Ref.current.children, ...container2Ref.current.children];
 
-            if (top < window.innerHeight && bottom > 0) {
-               e.classList.add('floatUp')
-               e.classList.add('appeared')
-            }
-         })
+    let allAppeared = true;
 
-         let countAppeared = 0
-         elements.forEach(e => {
-            if (e.className.includes('appeared')) {
-               countAppeared++
-            }
-         })
-
-         if (countAppeared === elements.length) {
-            console.log('remove---BlogFeed')
-            window.removeEventListener('scroll', handleScroll)
-         }
+    elements.forEach((e) => {
+      const { top, bottom } = e.getBoundingClientRect();
+      if (top < window.innerHeight && bottom > 0) {
+        e.classList.add('floatUp');
+        e.classList.add('appeared');
       }
-   }, [])
-
-   useEffect(() => {
-      handleScroll()
-      window.addEventListener('scroll', handleScroll)
-      return () => {
-         window.removeEventListener('scroll', handleScroll)
+      if (!e.classList.contains('appeared')) {
+        allAppeared = false;
       }
-   }, [handleScroll])
+    });
 
-   return (
-      <section style={{ padding: '40px 0' }}>
-         <div ref={container1Ref}>
-            {data.map(blog => (
-               <div key={blog.id}>{blog.title}</div> // simple rendering for now
-            ))}
-         </div>
-         <div ref={container2Ref}>
-            {/* You can render more here */}
-         </div>
-      </section>
-   )
+    if (allAppeared) {
+      console.log('remove---BlogFeed');
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, []);
+
+  useEffect(() => {
+    handleScroll();
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [handleScroll]);
+
+  return (
+    <section style={{ padding: '40px 0' }}>
+      <div ref={container1Ref}>
+        {data.map((blog) => (
+          <div key={blog.id} className="blog-item">
+            {blog.title}
+          </div>
+        ))}
+      </div>
+      <div ref={container2Ref}>
+        {/* Optional: put additional content here */}
+      </div>
+    </section>
+  );
 }
 
-export default BlogFeed
+export default BlogFeed;
