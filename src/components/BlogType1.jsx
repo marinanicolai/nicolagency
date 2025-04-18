5eb83a1aeb7dbd3687b4ddc937f65bc8c52fb99c
Marina Nicolai <nicolaimarina@gmail.com>
2025-04-18 11:23:06 -0400

    adding the first blog info

--- src/components/BlogType1.jsx
+++ src/components/BlogType1.jsx
@@ -1,8 +1,96 @@
+import { faMessage } from '@fortawesome/free-regular-svg-icons'
+import { faShareAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { memo } from 'react'
 import { useNavigate } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import styles from './styles/BlogType1.module.scss'
 
-const BlogType1 = ({data, myArea}) => {
-    const navigate = useNavigate();
-    return <div onClick={() => navigate(`/blogs/${data.id}`)}>Blog Type 1</div>;
-  };
-  
-  export default BlogType1;
+function BlogType1({ data, myArea }) {
+   const navigate = useNavigate()
+
+   // Ensure data and styles are defined
+  if (!data || !data.styles) {
+    return <div>Invalid Blog Data</div>;
+  }
+
+   let { visual, small, extraSmall, square, squareSmall, noOverlayContent } = data.styles || {}
+   if (extraSmall) {
+      visual = true
+   }
+
+   return (
+      <div
+         className={`${styles.blogType1} ${myArea ? styles.myArea : ''} ${small ? styles.small : ''} ${
+            extraSmall ? styles.extraSmall : ''
+         } ${square ? styles.square : ''} ${squareSmall ? styles.squareSmall : ''} ${
+            visual ? styles.visual : ''
+         }`}
+         onClick={() => navigate('/blogs/1')}
+      >
+         <div className={styles.blogType1Container}>
+            <div className={`${styles.thumbnail} ${visual ? styles.visual : ''}`}>
+               <img src={data.image} alt='blog' />
+
+               <div className={styles.overlay}>
+                  <div className={styles.iconWrap}>
+                     <div className={styles.icon}>
+                        <FontAwesomeIcon icon={faShareAlt} />
+                     </div>
+                     <div className={styles.icon}>
+                        <FontAwesomeIcon icon={faMessage} />
+                        <span>{data.comments}</span>
+                     </div>
+                  </div>
+               </div>
+
+               <div
+                  className={`${styles.slideContent} ${
+                     noOverlayContent ? styles.noOverlayContent : ''
+                  } ${visual ? styles.visual : ''}`}
+               >
+                  <div className={styles.content}>
+                     <p className={styles.date}>{data.date}</p>
+                     <h4 className={styles.title}>{data.title}</h4>
+                  </div>
+                  <div className={styles.author}>
+                     <img src={data.avatar} alt='avatar' />
+                     <span>{data.author}</span>
+                     <p className={styles.desc}>{data.desc}</p>
+                  </div>
+               </div>
+            </div>
+
+            <div className={`${styles.content} ${visual ? styles.visual : ''}`}>
+               <p className={styles.date}>{data.date}</p>
+               <h4 className={styles.title}>{data.title}</h4>
+            </div>
+         </div>
+      </div>
+   )
+}
+BlogType1.propTypes = {
+  data: PropTypes.shape({
+     id: PropTypes.number.isRequired,
+     image: PropTypes.string,
+     styles: PropTypes.shape({
+        visual: PropTypes.bool,
+        small: PropTypes.bool,
+        extraSmall: PropTypes.bool,
+        square: PropTypes.bool,
+        squareSmall: PropTypes.bool,
+        noOverlayContent: PropTypes.bool,
+     }),
+     date: PropTypes.string,
+     title: PropTypes.string,
+     comments: PropTypes.number,
+     avatar: PropTypes.string,
+     author: PropTypes.string,
+     desc: PropTypes.string,
+  }).isRequired,
+  myArea: PropTypes.bool,
+};
+
+
+
+export default memo(BlogType1);
--- src/reducers/blogReducer.js
+++ src/reducers/blogReducer.js
@@ -1,24 +1,42 @@
 import { act } from "react";
 
-const initialState = {
-    blogs: [],
-    blogFeed: [],
+import blogThumb1 from "../assets/images/deploy.jpg";
+
+import authorAvt1 from '../assets/images/avatar_profile.png'
+
+
+
+const initState = {
+    blogs: [ {
+      id: 1,
+      image: blogThumb1,
+      author: 'Marina Nicolai',
+      avatar: authorAvt1,
+      title: 'Deploy your app in 5 minutes',
+      desc: '«Sharing is the healthiest addiction»',
+      date: 'April 18th, 2025',
+      createdAt: 1686735683881,
+      comments: 0,
+      likes: 0,
+      categories: ['Marina Nicolai', 'Agency'],
+      styles: {},
+      type: 1,
+   }],
+    blogFeed: [{ blogId: 1, type: 1, styles: {} }],
     blogFeed2: [],
   };
+
+
+
   
-  export default function blogsReducer(state = initialState, action) {
-    console.log('Action:', action);
-    console.log('State before:', state);
-  
+  function blogReducer(state = initState, action) {
     switch (action.type) {
-      case 'SET_BLOGS':
-        const newState = {
-          ...state,
-          ...action.payload,
-        };
-        console.log('State after:', newState);
-        return newState;
-      default:
-        return state;
+       case '':
+          return state
+ 
+       default:
+          return state
     }
-  }
+ }
+ 
+ export default blogReducer
