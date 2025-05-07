20cbf8d789a0fc81a76642fc6ff05e38844e8f1f
Marina Nicolai <nicolaimarina@gmail.com>
2025-05-07 15:35:01 -0400

    add the service level 2 pages

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
             <Route path='/clients' element={<ClientsPage />} />
             <Route path='/about' element={<AboutPage />} />
             <Route path='/services' element={<ServicesPage />} />
+            <Route path='/services/:serviceName' element={<ServicesPage />} />
             <Route path='/projects' element={<ProjectsPage />} />
             <Route path='/blogs/:id' element={<BlogPage />} />
             <Route path='/blogs/categories/:id' element={<BlogPage />} />
